0a890c01c36a5500ff67cdc9fac95c617f7f1d98
Eduard <terriblegoodday@icloud.com>
2019-11-23 16:43:18 +1000

    Initial Commit

---
+++ Array Sum OOP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Array Sum OOP
+//
+//  Created by Eduard Dzhumagaliev on 11/23/19.
+//  Copyright © 2019 Eduard Dzhumagaliev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
